4e8d6a7c3c7538e7800009dbe2e0ddecaf79f5ec
TJ-Feledy <teej387@yahoo.com>
2020-03-03 20:10:19 -0600

    Working on max_profit

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python
-
+"""
+- receives a list
+- return max profit
+- must buy before sell (number to sell must be after number to buy, in the list)
+for each price in the list:
+  -find the difference between it and each price before it
+  -check if current difference is > the last diff.:
+    -if its greater, save it in a variable
+return the biggest profit.
+"""
 import argparse
 
 def find_max_profit(prices):
-  pass
+  max_profit = 0
+  index = 0
+  for i in range(index, len(prices)):
+    while i < index:
+      diff = 0
+  
 
 
 if __name__ == '__main__':
